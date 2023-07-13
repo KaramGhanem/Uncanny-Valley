a2a8acb246bb11c4d5095b5c633b3f55ef037a04
Karam Ghanem <ghanemkaram12@gmail.com>
2023-07-13 17:50:09 -0400

    added sampling command to shell script

---
+++ score_sde_pytorch/SDE.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --partition=unkillable                           # Ask for unkillable job
+#SBATCH --cpus-per-task=8                                # Ask for 8 CPUs
+#SBATCH --gres=gpu:2                                     # Ask for 2 GPU
+#SBATCH --mem=48G                                        # Ask for 48 GB of RAM
+#SBATCH -o /network/scratch/k/karam.ghanem/slurm-%j.out  # Write the log on scratch
+
+# 1. Load the required modules
+module --quiet load miniconda/3
+
+# 2. Load your environment
+conda activate edm
+module load cuda/11.7
+
+# 3. Copy your dataset on the compute node
+#cp /home/mila/k/karam.ghanem/Diffusion/cifar10_png/train /network/datasets/cifar10_train $SLURM_TMPDIR
+
+# 4. Launch your job, tell it to save the model in $SLURM_TMPDIR
+#    and look for the dataset into $SLURM_TMPDIR
+
+#Imagenet
+
+#CIFAR10
+
+#NEED TO CHANGE THE CONFIG FILE (INVESIGATE VARIOUS VP/VE CONFIGURATIONS)
+python /network/scratch/k/karam.ghanem/Diffusion/minDiffusion/score_sde_pytorch/main.py --config /network/scratch/k/karam.ghanem/Diffusion/minDiffusion/score_sde_pytorch/configs/ve/ncsn/cifar10.py --eval_folder /network/scratch/k/karam.ghanem/Diffusion/minDiffusion/score_sde_pytorch/ncsn_results --mode "train" --workdir /network/scratch/k/karam.ghanem/Diffusion/minDiffusion/score_sde_pytorch
+
+# 5. Copy whatever you want to save on $SCRATCH
+cp $SLURM_TMPDIR  /network/scratch/k/karam.ghanem
+
+
